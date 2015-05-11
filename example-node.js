25d7d45c3a3929a21d9e14ee451daedd45b49981
Mario Aichinger <aichingm@gmail.com>
2015-05-11 21:11:42 +0200

    added an example for node.js

---
+++ example-node.js
@@ -0,0 +1,23 @@
+/*global require*/
+/*global console*/
+
+var DIJKSTRA = require("./src/dijkstra.js");
+
+var graph1 = new DIJKSTRA.Dijkstra({a: {b: 2}, b: {a: 3, c: 48}, c: {b: 4}});
+console.log(graph1.getPath("a", "c"));
+console.log(graph1);
+
+var graph2 = new DIJKSTRA.Dijkstra({
+    "Frankfurt": {"Kassel": 173, "Mannheim": 85, "Würzburg": 217},
+    "Mannheim": {"Frankfurt": 85, "Karlsruhe": 80},
+    "Karlsruhe": {"Mannheim": 80, "Augsburg": 250},
+    "Augsburg": {"Karlsruhe": 250, "München": 84},
+    "München": {"Augsburg": 84, "Kassel": 502, "Nürnberg": 167},
+    "Kassel": {"Frankfurt": 173, "München": 502},
+    "Nürnberg": {"Würzburg": 103, "München": 167, "Stuttgart": 183},
+    "Stuttgart": {"Nürnberg": 183},
+    "Erfurt": {"Würzburg": 186},
+    "Würzburg": {"Erfurt": 186, "Nürnberg": 103, "Frankfurt": 217}
+});
+console.log(graph2.getPath("Frankfurt", "München"));
+console.log(graph2);
